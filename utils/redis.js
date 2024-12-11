986ca424159d87e7e8b4a21f009470f37f11e39e
adoumasseo <adoumasseo@gmail.com>
2024-12-11 12:19:44 +0100

    redis config of file manager

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+const { createClient } = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.log('Redis Client Error', err));
+  }
+
+  async isAlive() {
+    try {
+      if (this.client.isOpen) {
+        await this.client.connect();
+      }
+      const response = await this.client.ping();
+      return response === 'PONG';
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async get(key) {
+    const value = await this.client.get(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value, { EX: duration });
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = RedisClient();
+module.exports = redisClient;
